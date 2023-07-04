4747924df9f36fa4d1ae22e968d01486ef15ae30
exir180 <63543566+exir180@users.noreply.github.com>
2023-07-04 22:16:34 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,98 +1,173 @@
 import discord
 from discord.ext import commands, tasks
-import openpyxl
 import random
 from itertools import cycle
 import os
-from openpyxl import load_workbook
 import asyncio
+import requests
+from datetime import timezone
+import datetime
+import base64
 
-wb = load_workbook(r'C:\Users\pablo\PycharmProjects\trivia_bot\trivia.xlsx')
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
-sheet = wb.active
 counterA = 0
 counterB = 0
 counterC = 0
 counterD = 0
-def question(choice):
-    question = sheet[f"A{choice}"].value
-    return question
-def choiceA(choice):
-    choiceA = sheet[f"B{choice}"].value
-    return choiceA
-def choiceB(choice):
-    choiceB = sheet[f"C{choice}"].value
-    return choiceB
-def choiceC(choice):
-    choiceC = sheet[f"D{choice}"].value
-    return choiceC
-def choiceD(choice):
-    choiceD = sheet[f"E{choice}"].value
-    return choiceD
-def answer(choice):
-    answer = sheet[f"F{choice}"].value
-    return answer
+numberr = 0
 
 @bot.command()
 async def trivia(ctx):
     global number
+    global counterA
+    global counterB
+    global counterC
+    global counterD
+    counterA = 0
+    counterB = 0
+    counterC = 0
+    counterD = 0
     print("in trivia")
     member = ctx.message.author
-    numberrr = random.randint(1, 150)
-    questionN = question(numberrr)
-    choiceeA = choiceA(numberrr)
-    choiceeB = choiceB(numberrr)
-    choiceeC = choiceC(numberrr)
-    choiceeD = choiceD(numberrr)
-    answerR = answer(numberrr)
-    embed_message = discord.Embed(title="Trivia",
-                            description="Trivia question answer by doing .......",
-                            color=0x109319)
-    embed_message.add_field(name=questionN, value="A = " + choiceeA + "\nB = " + choiceeB + "\nC = " + choiceeC + "\nD = " + choiceeD + "",
-                    inline=False)
+    resp = requests.get('https://opentdb.com/api.php?amount=1&type=multiple&encode=base64')
+    parsed = resp.json()
 
-    embed_message.set_footer(text="this bot has been made by <@698854159117582439>")
-    await ctx.send(embed=embed_message)
-    print("before count")
+    for result in parsed["results"]:
+        questionn = result["question"]
+        base64_message = questionn
+        base64_bytes = base64_message.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        question = message_bytes.decode('ascii')
+        print(question)
+    for result in parsed["results"]:
+        choiceeA = result["correct_answer"]
+        base64_message = choiceeA
+        base64_bytes = base64_message.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        choiceA = message_bytes.decode('ascii')
+    for result in parsed["results"]:
+        wronganswerss = result["incorrect_answers"]
+        base64_message = wronganswerss
+        base64_bytes = base64_message.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        wronganswers = message_bytes.decode('ascii')
+    for result in parsed["results"]:
+        answerr = result["correct_answer"]
+        base64_message = answerr
+        base64_bytes = base64_message.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        answer = message_bytes.decode('ascii')
+    numberr = random.randint(1,4)
+    choiceB, choiceC, choiceD = wronganswers
+    await ctx.send(answer)
+    if numberr == 1:
+        print("1")
+        embed_message = discord.Embed(title="Trivia",
+                                      description="Trivia question answer by doing .......",
+                                      color=0x109319)
+        embed_message.add_field(name=question,
+                                value="A = " + choiceA + "\nB = " + choiceB + "\nC = " + choiceC + "\nD = " + choiceD + "",
+                                inline=False)
+
+        embed_message.set_footer(text="this bot has been made by <@698854159117582439>")
+        await ctx.send(embed=embed_message)
+    elif numberr == 2:
+        print("2")
+        embed_message = discord.Embed(title="Trivia",
+                                      description="Trivia question answer by doing .......",
+                                      color=0x109319)
+        embed_message.add_field(name=question,
+                                value="A = " + choiceB + "\nB = " + choiceA + "\nC = " + choiceC + "\nD = " + choiceD + "",
+                                inline=False)
+
+        embed_message.set_footer(text="this bot has been made by <@698854159117582439>")
+        await ctx.send(embed=embed_message)
+    elif numberr == 3:
+        print("3")
+        embed_message = discord.Embed(title="Trivia",
+                                      description="Trivia question answer by doing .......",
+                                      color=0x109319)
+        embed_message.add_field(name=question,
+                                value="A = " + choiceC + "\nB = " + choiceB + "\nC = " + choiceA + "\nD = " + choiceD + "",
+                                inline=False)
+
+        embed_message.set_footer(text="this bot has been made by <@698854159117582439>")
+        await ctx.send(embed=embed_message)
+    elif numberr == 4:
+        print("4")
+        embed_message = discord.Embed(title="Trivia",
+                                      description="Trivia question answer by doing .......",
+                                      color=0x109319)
+        embed_message.add_field(name=question,
+                                value="A = " + choiceD + "\nB = " + choiceB + "\nC = " + choiceC + "\nD = " + choiceA + "",
+                                inline=False)
+
+        embed_message.set_footer(text="this bot has been made by <@698854159117582439>")
+        await ctx.send(embed=embed_message)
     await asyncio.sleep(6)
-    print("after count")
-    print(counterA)
-    print(counterB)
-    print(counterC)
-    print(counterD)
-    if counterA > counterB and counterA > counterC and counterA  > counterD:
-        await ctx.send("you guys chose A")
-    elif counterB > counterA and counterB > counterB and counterC > counterD:
-        await ctx.send("you guys chose B")
-    elif counterC > counterA and counterC > counterB and counterC > counterD:
-        await ctx.send("you guys chose C")
-    elif counterD > counterA and counterD > counterB and counterD > counterC:
-        await ctx.send("you guys chose D")
-    elif counterA == 0:
-        await ctx.send("error contant exir180")
+    dt = datetime.datetime.now(timezone.utc)
+    utc_time = dt.replace(tzinfo=timezone.utc)
+    utc_timestamp = utc_time.timestamp()
+    before = utc_timestamp * 1000
+
+
 
 
 @bot.command()
 async def vote(ctx, arg):
     print("voted")
+    global numberr
     global counterA
     global counterB
     global counterC
     global counterD
     if arg == "A":
-        await ctx.send("voted A")
+        await ctx.send("chose A")
         counterA = counterA + 1
+        dt = datetime.datetime.now(timezone.utc)
+        utc_time = dt.replace(tzinfo=timezone.utc)
+        utc_timestamp = utc_time.timestamp()
+        after = utc_timestamp * 1000
+        if numberr == 1:
+            await ctx.send("yup")
+        else:
+            await ctx.send("nope")
     elif arg == "B":
-        await ctx.send("voted B")
+        await ctx.send("chose B")
         counterB += 1
+        dt = datetime.datetime.now(timezone.utc)
+        utc_time = dt.replace(tzinfo=timezone.utc)
+        utc_timestamp = utc_time.timestamp()
+        after = utc_timestamp * 1000
+        if numberr == 2:
+            await ctx.send("yup")
+        else:
+            await ctx.send("nope")
     elif arg == "C":
-        await ctx.send("voted C")
+        await ctx.send("chose C")
         counterC += 1
+        dt = datetime.datetime.now(timezone.utc)
+        utc_time = dt.replace(tzinfo=timezone.utc)
+        utc_timestamp = utc_time.timestamp()
+        after = utc_timestamp * 1000
+        if numberr == 3:
+            await ctx.send("yup")
+        else:
+            await ctx.send("nope")
     elif arg == "D":
-        await ctx.send("voted D")
+        await ctx.send("chose D")
         counterD += 1
+        dt = datetime.datetime.now(timezone.utc)
+        utc_time = dt.replace(tzinfo=timezone.utc)
+        utc_timestamp = utc_time.timestamp()
+        after = utc_timestamp * 1000
+        if numberr == 4:
+            await ctx.send("yup")
+        else:
+            await ctx.send("nope")
+    PointsGain = after
 
 
-bot.run("MTExNzUzODAyNjA3NjI0MjA4NQ.GOx9nL.VsN6kiGP8Xw7b0CGt2MfOG_hdeh6TslpmggDps")
+bot.run("token")
